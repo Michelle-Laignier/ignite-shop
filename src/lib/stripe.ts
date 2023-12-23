2fe01fcd3869015ef4e9b55f265329a39cf1ac52
Laignier <michellelaignier17@gmail.com>
2023-12-23 14:16:49 -0300

    fix: lib/stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,17 @@
-import Stripe from 'stripe'
+import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error('Stripe secret key is not defined');
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2023-10-16',
   appInfo: {
     name: 'Ignite Shop',
-  }
-})
+  },
+});
 
 // appInfo/name: pra todas as chamadas feitas no Stripe ficarem com
 // um log e aparecer o nome da aplicação que fez a requisição.

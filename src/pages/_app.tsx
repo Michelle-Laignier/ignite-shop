732b8c81e0a02fa918ccb52d2e27fad814ae3450
Laignier <michellelaignier17@gmail.com>
2024-01-08 12:55:29 -0300

    header img: Link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { globalStyles } from "../styles/global"
 import { Container, Header } from "../styles/pages/app"
 
 import logo from "../../src/assets/logo.svg"
+import Link from "next/link"
 
 globalStyles()
 
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <Container>
       <Header>
-        <Image src={logo} alt="" priority={true}/>
+        <Link href="/">
+          <Image src={logo} alt="" priority={true}/>
+        </Link>
       </Header>
 
       <Component {...pageProps} />
